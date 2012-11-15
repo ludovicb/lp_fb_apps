1eeb54e7a4b91c49cdfd2eecf7b22f3153001cfe
ludovic jean marc barbot <ludobarbot@yahoo.co.uk>
2012-11-15 09:43:51 +0400

    deleted bad html added new good ones

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	
+<title>
+Wufoo : I LOVE MY REGION
+</title>
+
+<!-- Meta Tags -->
+<meta charset="utf-8">
+<meta name="generator" content="Wufoo">
+<meta name="robots" content="index, follow">
+
+<style>
+
+  *{margin:0;padding:0;}
+  html{overflow:hidden;}
+  #wufoo{position:absolute;width:100%;height:100%;}
+  body{margin-bottom:-17px;}
+  iframe{height:100%;width:100%;border:none;}
+
+</style>
+
+</head>
+
+<body>
+	
+<div id="wufoo">
+<iframe title="I LOVE MY REGION" frameborder="0" src="http://ludovicbarbot.wufoo.com/forms/z7x3p9/">
+<a href="http://ludovicbarbot.wufoo.com/forms/z7x3p9/" title="HTML Form">I LOVE MY REGION</a>
+</iframe>
+</div>
+
+
+</body>
+</html>
